4fc08972436afe60072d2369618be78dae6f831b
Fluxticks <benjigarment.appdev@gmail.com>
2020-11-25 03:26:24 +0000

    Added a hint to scroll in the help screen.

--- core/src/com/dragonboatrace/screens/HelpScreen.java
+++ core/src/com/dragonboatrace/screens/HelpScreen.java
@@ -3,9 +3,13 @@ package com.dragonboatrace.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Vector2;
 import com.dragonboatrace.entities.Button;
 import com.dragonboatrace.tools.Settings;
@@ -56,6 +60,21 @@ public class HelpScreen implements Screen {
     private int currPos;
 
     /**
+     * Scroll down indicator.
+     */
+    private final Texture downArrow;
+
+    /**
+     * GlyphLayout used for centering fonts
+     */
+    private final GlyphLayout layout;
+
+    /**
+     * The font used to display the scroll indicator.
+     */
+    private final BitmapFont font;
+
+    /**
      * Creates a new window to display the help info in.
      *
      * @param callBack A reference to the main menu window so we can return using the back button.
@@ -69,6 +88,16 @@ public class HelpScreen implements Screen {
         this.currPos = maxHeight;
         this.padding = 200 / Settings.SCALAR;
         this.minHeight = padding;
+
+        this.downArrow = new Texture(Gdx.files.local("down_arrow.png"));
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("osaka-re.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size *= 3.0 / Settings.SCALAR;
+        parameter.color = Color.RED;
+        this.font = generator.generateFont(parameter);
+        this.layout = new GlyphLayout();
+        layout.setText(font, "Scroll Down");
 
         /* Set up the ability to scroll the image */
         Gdx.input.setInputProcessor(new InputProcessor() {
@@ -146,6 +175,10 @@ public class HelpScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(this.helpInfo, padding, currPos, Gdx.graphics.getWidth() - 2 * padding, (float) this.helpInfo.getHeight() / Settings.SCALAR);
+        if(currPos==this.maxHeight){
+            font.draw(batch,"Scroll Down",(Gdx.graphics.getWidth() - layout.width) / 2, layout.height + 120f/Settings.SCALAR);
+            batch.draw(this.downArrow, Gdx.graphics.getWidth() / 2f - 50f/Settings.SCALAR, 0, 100f/Settings.SCALAR, 100f/Settings.SCALAR);
+        }
         backButton.render(batch);
         if (this.backButton.isHovering() && Gdx.input.isTouched()) {
             this.mainMenu.game.setScreen(this.mainMenu);
