c0fd3b6ca2fe11efd7965bd948e29c64595f5cbf
wpw503 <wpw503@york.ac.uk>
2021-01-29 16:25:55 +0000

    testing moved

---
+++ tests/src/com/dragonboatrace/tools/ScrollingBackgroundTest.java
@@ -0,0 +1,12 @@
+package com.dragonboatrace.tools;
+
+import junit.framework.TestCase;
+
+public class ScrollingBackgroundTest extends TestCase {
+
+    public void testUpdate() {
+    }
+
+    public void testResize() {
+    }
+}
