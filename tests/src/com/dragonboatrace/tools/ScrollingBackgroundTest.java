6625f123572a8324b1d48e157994a58a01919a9e
wpw503 <wpw503@york.ac.uk>
2021-01-29 16:44:19 +0000

    private acces

--- tests/src/com/dragonboatrace/tools/ScrollingBackgroundTest.java
+++ tests/src/com/dragonboatrace/tools/ScrollingBackgroundTest.java
@@ -1,12 +1,34 @@
 package com.dragonboatrace.tools;
 
+import de.tomgrill.gdxtesting.GdxTestRunner;
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.lang.reflect.Field;
+
+@RunWith(GdxTestRunner.class)
 public class ScrollingBackgroundTest extends TestCase {
 
-    public void testUpdate() {
+    @Test
+    public void testUpdate() throws NoSuchFieldException, IllegalAccessException {
+        ScrollingBackground s = new ScrollingBackground();
+
+        Field fy1 = ScrollingBackground.class.getDeclaredField("y1");
+        Field fy2 = ScrollingBackground.class.getDeclaredField("y2");
+        Field fxPosition = ScrollingBackground.class.getDeclaredField("xPosition");
+
+        fy1.setAccessible(true);
+        fy2.setAccessible(true);
+        fxPosition.setAccessible(true);
+
+
+        float y1 = (float) fy1.get(s);
+        float y2 = (float) fy2.get(s);
+        float xPosition = (float) fxPosition.get(s);
     }
 
+    @Test
     public void testResize() {
     }
 }
