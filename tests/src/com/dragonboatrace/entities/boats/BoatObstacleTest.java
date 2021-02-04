7b3dbd020fce87486ed438f39d31fe603f838d49
wpw503 <wpw503@york.ac.uk>
2021-02-04 16:58:01 +0000

    manual test failing

--- tests/src/com/dragonboatrace/entities/boats/BoatObstacleTest.java
+++ tests/src/com/dragonboatrace/entities/boats/BoatObstacleTest.java
@@ -70,7 +70,7 @@ public class BoatObstacleTest {
         //System.out.println(initialHealth);
         //System.out.println(b.getHealth());
 
-        Assert.assertTrue(initialHealth < b.getHealth());
+        Assert.assertTrue(initialHealth > b.getHealth());
     }
 
     // These tests have been verified in every members IDE however they fail on github so assertions have been removed.
@@ -88,6 +88,6 @@ public class BoatObstacleTest {
         //System.out.println(race.getPlayer().position.x);
         //System.out.println(race.getPlayer().position.y);
 
-        Assert.assertTrue(game.getScreen() instanceof GameOverScreen);
+        if(game.getScreen() instanceof GameOverScreen) Assert.fail();
     }
 }
