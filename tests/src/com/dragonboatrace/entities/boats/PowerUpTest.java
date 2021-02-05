a92e86d8cfb41adc17dd29f05b715fc750099230
Umer Fakher <66469756+UmerFakher@users.noreply.github.com>
2021-02-05 20:39:13 +0000

    Update PowerUpTest.java

--- tests/src/com/dragonboatrace/entities/boats/PowerUpTest.java
+++ tests/src/com/dragonboatrace/entities/boats/PowerUpTest.java
@@ -49,136 +49,136 @@ public class PowerUpTest {
     @Test
     public void healthPowerUpBoatTest() {
         //Original:
-//        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
-//
-//        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
-//        float initialValue = b.getHealth();
-//        b.checkCollisions(new ArrayList<Obstacle>() {{
-//            add(new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0, 100)));
-//        }});
-//
-//        Assert.assertTrue(initialValue < b.getHealth());
-
-        //testing:
-
         Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
-
-        Obstacle o = new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0,100)); // 100 was y
-        l.getObstacles().add(o);
-//        l.update(1,5);
 
         Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
         float initialValue = b.getHealth();
-        final float y = b.position.y;
-
-        System.out.println(b.lane.getObstacles().size());
-        System.out.println(l.getObstacles().size());
-
-//        b.checkCollisions(new ArrayList<Obstacle>(){{add(new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0,y)));}});
-
-
-
-//        b.checkCollisions(new ArrayList<Obstacle>(){{add(o);}});
-        b.checkCollisions();
-
-
-
-        System.out.println(b.lane.getObstacles().size());
-        System.out.println(l.getObstacles().size());
-
-//        boolean t = b.getHitBox().collidesWith((new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0,y))).getHitBox());
-//        System.out.println(t);
-
-        //System.out.println(b.position.x);
-        //System.out.println(b.position.y);
-
-        //System.out.println(initialHealth);
-        //System.out.println(b.getHealth());
-
-//        Obstacle p = new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0, 0));
-//        System.out.println(l.getObstacles().size());
-//
-//        System.out.println(b.checkCollisions(l.getObstacles()));
-//
-//        System.out.println(l.getObstacles().size());
-
-        System.out.println();
-        System.out.println(initialValue);
-        System.out.println(b.getHealth());
+        b.checkCollisions(new ArrayList<Obstacle>() {{
+            add(new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0, 100)));
+        }});
 
         Assert.assertTrue(initialValue < b.getHealth());
+
+        //testing:
+
+//        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
+//
+//        Obstacle o = new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0,100)); // 100 was y
+//        l.getObstacles().add(o);
+////        l.update(1,5);
+//
+//        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
+//        float initialValue = b.getHealth();
+//        final float y = b.position.y;
+//
+//        System.out.println(b.lane.getObstacles().size());
+//        System.out.println(l.getObstacles().size());
+//
+////        b.checkCollisions(new ArrayList<Obstacle>(){{add(new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0,y)));}});
+//
+//
+//
+////        b.checkCollisions(new ArrayList<Obstacle>(){{add(o);}});
+//        b.checkCollisions();
+//
+//
+//
+//        System.out.println(b.lane.getObstacles().size());
+//        System.out.println(l.getObstacles().size());
+//
+////        boolean t = b.getHitBox().collidesWith((new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0,y))).getHitBox());
+////        System.out.println(t);
+//
+//        //System.out.println(b.position.x);
+//        //System.out.println(b.position.y);
+//
+//        //System.out.println(initialHealth);
+//        //System.out.println(b.getHealth());
+//
+////        Obstacle p = new Obstacle(ObstacleType.PU_HEALTH, new Vector2(0, 0));
+////        System.out.println(l.getObstacles().size());
+////
+////        System.out.println(b.checkCollisions(l.getObstacles()));
+////
+////        System.out.println(l.getObstacles().size());
+//
+//        System.out.println();
+//        System.out.println(initialValue);
+//        System.out.println(b.getHealth());
+//
+//        Assert.assertTrue(initialValue < b.getHealth());
     }
 
 
 
-//    /**
-//     * Test to see if a boat colliding with a stamina power-up will increase its stamina correctly.
-//     */
-//    @Test
-//    public void staminaPowerUpBoatTest() {
-//        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
-//
-//        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
-//        float initialValue = b.getStamina();
-//        b.checkCollisions(new ArrayList<Obstacle>() {{
-//            add(new Obstacle(ObstacleType.PU_STAMINA, new Vector2(0, 100)));
-//        }});
-//
-//        Assert.assertTrue(initialValue < b.getStamina());
-//    }
-//
-//
-//    /**
-//     * Test to see if a boat colliding with an agility power-up will increase its agility correctly.
-//     */
-//    @Test
-//    public void agilityPowerUpBoatTest() {
-//        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
-//
-//        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
-//        float initialValue = b.getAgility();
-//        b.checkCollisions(new ArrayList<Obstacle>() {{
-//            add(new Obstacle(ObstacleType.PU_AGILITY, new Vector2(0, 100)));
-//        }});
-//
-//        Assert.assertTrue(initialValue < b.getAgility());
-//    }
-//
-//    /**
-//     * Test to see if a boat colliding with a speed power-up will increase its speed correctly.
-//     */
-//    @Test
-//    public void speedPowerUpBoatTest() {
-//        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
-//
-//        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
-//        float initialValue = b.getSpeed();
-//        b.checkCollisions(new ArrayList<Obstacle>() {{
-//            add(new Obstacle(ObstacleType.PU_SPEED, new Vector2(0, 100)));
-//        }});
-//
-//        Assert.assertTrue(initialValue < b.getSpeed());
-//    }
-//
-//    /**
-//     * Test to see if a boat colliding with an ALL power-up will increase its health, stamina, agility and speed correctly.
-//     */
-//    @Test
-//    public void allPowerUpBoatTest() {
-//        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
-//
-//        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
-//        float initialH = b.getHealth();
-//        float initialSta = b.getStamina();
-//        float initialAgi = b.getAgility();
-//        float initialSpe = b.getSpeed();
-//        b.checkCollisions(new ArrayList<Obstacle>() {{
-//            add(new Obstacle(ObstacleType.PU_ALL, new Vector2(0, 100)));
-//        }});
-//
-//        Assert.assertTrue(initialH < b.getHealth());
-//        Assert.assertTrue(initialSta < b.getStamina());
-//        Assert.assertTrue(initialAgi < b.getAgility());
-//        Assert.assertTrue(initialSpe < b.getSpeed());
-//    }
+    /**
+     * Test to see if a boat colliding with a stamina power-up will increase its stamina correctly.
+     */
+    @Test
+    public void staminaPowerUpBoatTest() {
+        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
+
+        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
+        float initialValue = b.getStamina();
+        b.checkCollisions(new ArrayList<Obstacle>() {{
+            add(new Obstacle(ObstacleType.PU_STAMINA, new Vector2(0, 100)));
+        }});
+
+        Assert.assertTrue(initialValue < b.getStamina());
+    }
+
+
+    /**
+     * Test to see if a boat colliding with an agility power-up will increase its agility correctly.
+     */
+    @Test
+    public void agilityPowerUpBoatTest() {
+        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
+
+        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
+        float initialValue = b.getAgility();
+        b.checkCollisions(new ArrayList<Obstacle>() {{
+            add(new Obstacle(ObstacleType.PU_AGILITY, new Vector2(0, 100)));
+        }});
+
+        Assert.assertTrue(initialValue < b.getAgility());
+    }
+
+    /**
+     * Test to see if a boat colliding with a speed power-up will increase its speed correctly.
+     */
+    @Test
+    public void speedPowerUpBoatTest() {
+        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
+
+        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
+        float initialValue = b.getSpeed();
+        b.checkCollisions(new ArrayList<Obstacle>() {{
+            add(new Obstacle(ObstacleType.PU_SPEED, new Vector2(0, 100)));
+        }});
+
+        Assert.assertTrue(initialValue < b.getSpeed());
+    }
+
+    /**
+     * Test to see if a boat colliding with an ALL power-up will increase its health, stamina, agility and speed correctly.
+     */
+    @Test
+    public void allPowerUpBoatTest() {
+        Lane l = new Lane(new Vector2(0, 0), 50, 0, 0);
+
+        Boat b = new Boat(BoatType.FAST, l, "__testing_boat__");
+        float initialH = b.getHealth();
+        float initialSta = b.getStamina();
+        float initialAgi = b.getAgility();
+        float initialSpe = b.getSpeed();
+        b.checkCollisions(new ArrayList<Obstacle>() {{
+            add(new Obstacle(ObstacleType.PU_ALL, new Vector2(0, 100)));
+        }});
+
+        Assert.assertTrue(initialH < b.getHealth());
+        Assert.assertTrue(initialSta < b.getStamina());
+        Assert.assertTrue(initialAgi < b.getAgility());
+        Assert.assertTrue(initialSpe < b.getSpeed());
+    }
 }
