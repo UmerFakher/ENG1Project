486743cbb2fc3b8f2a19ddd28b81218cd01af96f
Umer Fakher <66469756+UmerFakher@users.noreply.github.com>
2021-02-09 15:02:07 +0000

    JavaDoc DocStrings and renamed
    ConfigurationTest, ScrollingBackgroundTest,
    AssestTest

--- tests/src/com/dragonboatrace/tools/ConfigurationTest.java
+++ tests/src/com/dragonboatrace/tools/ConfigurationTest.java
@@ -4,49 +4,70 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * TUA_SETTINGS - Check the default settings are correct.
+ *
+ * The default settings should be the same as in the design.
+ */
 public class ConfigurationTest {
 
+    /**
+     * Test that the resolution of the screen is as expected.
+     */
     @Test
-    public void setResolution() {
+    public void setResolutionTest() {
         Configuration.setResolution(10, 10);
         assertEquals(10, Configuration.WIDTH);
         assertEquals(10, Configuration.HEIGHT);
         assertEquals(192, Configuration.SCALAR);
     }
 
+    /**
+     * Test that window is fullscreen when it should be and shouldn't be.
+     */
     @Test
-    public void setFULLSCREEN() {
+    public void setFullscreenTest() {
         Configuration.setFULLSCREEN(true);
         assertTrue(Configuration.FULLSCREEN);
         Configuration.setFULLSCREEN(false);
         assertFalse(Configuration.FULLSCREEN);
-
-        // test that window is full screen?
     }
 
+    /**
+     * Test that the number of players, including the player itself, is as expected.
+     */
     @Test
-    public void setPlayerCount() {
+    public void setPlayerCountTest() {
         assertEquals(8, Configuration.PLAYER_COUNT);
         Configuration.setPlayerCount(30);
         assertEquals(30, Configuration.PLAYER_COUNT);
     }
 
+    /**
+     * Test that that the value that reduces the scalar that stamina gives when accelerating, is as expected.
+     */
     @Test
-    public void setStaminaSpeedDivision() {
+    public void setStaminaSpeedDivisionTest() {
         assertEquals(2, Configuration.STAMINA_SPEED_DIVISION);
         Configuration.setStaminaSpeedDivision(30);
         assertEquals(30, Configuration.STAMINA_SPEED_DIVISION);
     }
 
+    /**
+     *  Test that the velocity penalty given when a collision occurs, is as expected.
+     */
     @Test
-    public void setObstacleCollisionPenalty() {
+    public void setObstacleCollisionPenaltyTest() {
         assertEquals(-20, Configuration.OBSTACLE_COLLISION_PENALTY);
         Configuration.setObstacleCollisionPenalty(-10);
         assertEquals(-10, Configuration.OBSTACLE_COLLISION_PENALTY);
     }
 
+    /**
+     * Test that the time the boat must wait before moving again after a collision, is as expected.
+     */
     @Test
-    public void setObstacleCollisionTime() {
+    public void setObstacleCollisionTimeTest() {
         assertEquals(0.5f, Configuration.OBSTACLE_COLLISION_TIME, 0.001);
         Configuration.setObstacleCollisionTime(0.6f);
         assertEquals(0.6f, Configuration.OBSTACLE_COLLISION_TIME, 0.001);
